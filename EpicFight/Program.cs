6f27c08df176a9c3711ae534aa47da1886e96e02
Opilane TTHK <opilane@tthk.ee>
2021-10-21 09:53:23 +0300

    Week8-FunctionsWithReturns

---
+++ EpicFight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EpicFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero = RandomHero();
+            string villain = RandomVillain();
+            Console.WriteLine($"fight will be between {hero} and {villain}");
+        }
+
+        private static string RandomHero()
+        {
+            string[] heroes = {"Obama", "Mussoliini", "JFK", "Lenin", "Kanye", "Ubisoft"};
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, heroes.Length);
+
+            return heroes[randomIndex];
+        }
+
+        private static string RandomVillain()
+        {
+            string[] villains = {"George floyd", "Jooo_baiden", "Barrett M82", "turtle", };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, villains.Length);
+
+            return villains[randomIndex];
+        }
+    }
+}
---
+++ FunctionsWithReturns/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FunctionsWithReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] foods = { "Chicken wings", "burger", "french fries", "salad", "pizza" };
+            string[] drinks = { "coca cola", "fanta", "pepsi", "water" };
+
+            //DisplayRandomFromArray(foods);
+            //DisplayRandomFromArray(drinks);
+            string randomSnack = PickRandomFromArray(foods);
+            string randomDrink = PickRandomFromArray(drinks);
+
+
+
+            Console.WriteLine($"computer picked {randomSnack} and {randomDrink} for your movie night");
+        }
+
+        private static string PickRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            string randomElement = someArray[randomIndex];
+
+            return randomElement;
+        }
+
+        private static void DisplayRandomFromArray(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            Console.WriteLine($"computer has chosen {someArray[randomIndex]}");
+
+        }
+
+
+
+    }
+}
